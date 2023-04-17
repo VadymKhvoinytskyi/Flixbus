514e0793b38379644d7e4de6fabf61b630b3b1ec
Vadym Khvoinytskyi <khvojjnickijj@gmail.com>
2023-04-17 16:37:30 +0200

    try to fix result json

--- parse.py
+++ parse.py
@@ -20,8 +20,14 @@ arrival_uuids = ["40e086ed-8646-11e6-9066-549f350fcb0c"]
 
 dates = [f"{i}.05.2023" for i in range(10, 17)]
 
-for departure_uuid in departure_uuids:
-    for arrival_uuid in arrival_uuids:
+
+
+for arrival_uuid in arrival_uuids:
+
+    with open(f"to_{arrival_uuid}.json", "a") as file:
+        file.write('"result": [')
+
+    for departure_uuid in departure_uuids:
         for date in dates:
 
             response = requests.get(f"https://global.api.flixbus.com/search/service/v4/search?from_city_id={departure_uuid}&to_city_id={arrival_uuid}&departure_date={date}&products=%7B%22adult%22%3A2%7D&currency=EUR&locale=en&search_by=cities&include_after_midnight_rides=1")
@@ -33,6 +39,8 @@ for departure_uuid in departure_uuids:
                 file.write(json.dumps(response.json()["trips"][0]["results"]))
                 file.write(", ")
 
+with open(f"to_{arrival_uuid}.json", "a") as file:
+    file.write(']')
 
 
 '''response = requests.get("https://global.api.flixbus.com/search/service/v4/search?from_city_id=40de8964-8646-11e6-9066-549f350fcb0c&to_city_id=40d911c7-8646-11e6-9066-549f350fcb0c&departure_date=23.04.2023&products=%7B%22adult%22%3A2%7D&currency=EUR&locale=en&search_by=cities&include_after_midnight_rides=1")
